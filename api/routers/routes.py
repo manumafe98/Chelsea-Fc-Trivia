648ad882ca3a160d9bfc93e2badc6cf5163b27bf
manumafe98 <manumaxerapro@gmail.com>
2023-07-05 19:16:58 -0300

    Adding content for most_goals, most_appereances, position and reformating the way i return the data

--- api/routers/routes.py
+++ api/routers/routes.py
@@ -1,10 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+from fastapi.responses import RedirectResponse
 from db.client import db_client
 from db.models.model import ChelseaPlayers
 from db.schemas.schemas import chelsea_players_schema, chelsea_player_schema
 
 
 router = APIRouter()
+
+
+@router.get("/")
+async def home():
+    return RedirectResponse("/docs", status_code=status.HTTP_303_SEE_OTHER)
+
 
 @router.get("/players", response_model=list[ChelseaPlayers])
 async def players():
@@ -13,7 +20,7 @@ async def players():
     return chelsea_players_schema(random_players)
     
 
-@router.get("/nationality", response_model=list[ChelseaPlayers])
+@router.get("/nationality")
 async def nationality_question():
     # Which player of the followings is from: COUNTRY?
     random_data = db_client.chelsea_players.aggregate([{"$sample": {"size": 1}}])
@@ -23,16 +30,23 @@ async def nationality_question():
                                                           {"$sample": {"size": 2}}])
     players_array = chelsea_players_schema(random_players)
     players_array.append(random_player)
-    return players_array
+    return {"nationality": nationality, "corect_answer": random_player["name"], "players": players_array}
 
 
 @router.get("/position")
 async def position_question():
     # Which one of the following players used to play as: POSITION?
-    pass
+    random_data = db_client.chelsea_players.aggregate([{"$sample": {"size": 1}}])
+    random_player = chelsea_players_schema(random_data)[0]
+    position = random_player["position"]
+    random_players = db_client.chelsea_players.aggregate([{"$match": {"position": {"$ne": position}}}, 
+                                                          {"$sample": {"size": 2}}])
+    players_array = chelsea_players_schema(random_players)
+    players_array.append(random_player)
+    return {"position": position, "corect_answer": random_player["name"], "players": players_array}
 
 
-@router.get("/top_appearances", response_model=list[ChelseaPlayers])
+@router.get("/top_appearances")
 async def top_appearances():
     # Which one of these players has the most appearances for chelsea?
     sort_criteria = [("appearances", -1)]
@@ -42,10 +56,10 @@ async def top_appearances():
         {"$match": {"name": {"$ne": player_with_most_appearances_name}}}, {"$sample": {"size": 2}}])
     players_array = chelsea_players_schema(random_players)
     players_array.append(player_with_most_appearances)
-    return players_array
+    return {"corect_answer": player_with_most_appearances_name, "players": players_array}
 
 
-@router.get("/top_goalscorer", response_model=list[ChelseaPlayers])
+@router.get("/top_goalscorer")
 async def top_goalscorer():
     # Which one of these players is the top goalscorer of chelsea?
     sort_criteria = [("goals", -1)]
@@ -55,16 +69,56 @@ async def top_goalscorer():
         {"$match": {"name": {"$ne": player_with_most_goals_name}}}, {"$sample": {"size": 2}}])
     players_array = chelsea_players_schema(random_players)
     players_array.append(player_with_most_goals)
-    return players_array
+    return {"corect_answer": player_with_most_goals_name, "players": players_array}
 
 
 @router.get("/most_goals")
 async def most_goals():
     # Which player of the following have the most goals?
-    pass
+    random_data1 = db_client.chelsea_players.aggregate([{"$sample": {"size": 1}}])
+    random_player1 = chelsea_players_schema(random_data1)[0]
+
+    random_data2 = db_client.chelsea_players.aggregate([{"$match": {"goals": {"$ne": random_player1["goals"]}}}, 
+                                                        {"$sample": {"size": 1}}])
+    random_player2 = chelsea_players_schema(random_data2)[0]
+
+    random_data3 = db_client.chelsea_players.aggregate([{"$match": 
+                                                         {"$and": [{"goals": {"$ne": random_player1["goals"]}}, 
+                                                                   {"goals": {"$ne": random_player2["goals"]}}]}},
+                                                                   {"$sample": {"size": 1}}])
+    random_player3 = chelsea_players_schema(random_data3)[0]
+    player_array = [random_player1, random_player2, random_player3]
+    goals = 0
+    name = ""
+    for player in player_array:
+        if player["goals"] >= goals:
+            goals = player["goals"]
+            name = player["name"]
+    return {"goals": goals, "correct_answer": name, "players": player_array}
 
 
 @router.get("/most_appearances")
 async def most_appearances():
     # Which player of the following have the most appereances?
-    pass
+    random_data1 = db_client.chelsea_players.aggregate([{"$sample": {"size": 1}}])
+    random_player1 = chelsea_players_schema(random_data1)[0]
+
+    random_data2 = db_client.chelsea_players.aggregate([{"$match": {"appearances": 
+                                                                    {"$ne": random_player1["appearances"]}}}, 
+                                                                    {"$sample": {"size": 1}}])
+    random_player2 = chelsea_players_schema(random_data2)[0]
+
+    random_data3 = db_client.chelsea_players.aggregate([{"$match": 
+                                                         {"$and": 
+                                                          [{"appearances": {"$ne": random_player1["appearances"]}}, 
+                                                           {"appearances": {"$ne": random_player2["appearances"]}}]}}, 
+                                                           {"$sample": {"size": 1}}])
+    random_player3 = chelsea_players_schema(random_data3)[0]
+    player_array = [random_player1, random_player2, random_player3]
+    appearances = 0
+    name = ""
+    for player in player_array:
+        if player["appearances"] >= appearances:
+            appearances = player["appearances"]
+            name = player["name"]
+    return {"appearances": appearances, "correct_answer": name, "players": player_array}
