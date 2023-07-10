165294e94c6b3874c16b1eba7860b8f143e6bdb5
manumafe98 <manumaxerapro@gmail.com>
2023-07-10 19:17:14 -0300

    Changing to use tkinter instead of a frontend webpage

--- mainapp/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from routers import routes
-
-app = FastAPI()
-
-app.include_router(routes.router)
-app.mount("/static", StaticFiles(directory="static"), name="static")
