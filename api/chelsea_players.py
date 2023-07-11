04a4ad32547def72f71273779ef008af25113fa1
manumafe98 <manumaxerapro@gmail.com>
2023-07-11 15:43:42 -0300

    Using iloc because old usage will be depracated

--- api/chelsea_players.py
+++ api/chelsea_players.py
@@ -6,13 +6,13 @@ from db.models.model import ChelseaPlayer
 html = wp.page("List_of_Chelsea_F.C._players").html().encode("UTF-8")
 df = pd.read_html(html, encoding="UTF-8")[1]
 
-
 for index, row in df.iterrows():
-    new_player = ChelseaPlayer(name = str(row["Player"]),
-                               nationality = str(row["Nationality"]),
-                               position = str(row["Pos"]),
-                               career = str(row["Club career"]),
-                               appearances = row["Total"],
-                               goals = row["Goals"])
+
+    new_player = ChelseaPlayer(name = row.iloc[0],
+                               nationality = row.iloc[1],
+                               position = row.iloc[2],
+                               career = row.iloc[3],
+                               appearances = row.iloc[4],
+                               goals = row.iloc[5])
     data = new_player.dict()
     db_client.chelsea_players.insert_one(data)
